92163d73db4891c843d1163a3b0b60df42da5fa6
roberto.elero <roberto.elero@ifood.com.br>
2019-04-01 00:52:27 -0300

    First commit

---
+++ src/main/kotlin/robert/o/spring/jedis/JedisApplication.kt
@@ -0,0 +1,11 @@
+package robert.o.spring.jedis
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JedisApplication
+
+fun main(args: Array<String>) {
+	runApplication<JedisApplication>(*args)
+}
